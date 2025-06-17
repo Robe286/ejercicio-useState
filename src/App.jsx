1911e12dc37160cc335d22902aa9401b15c31195
Roberto Blasco~ <roberza2202@gmail.com>
2025-06-17 11:39:44 +0200

    ejercicio terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
 import './App.css'
+import { useState } from 'react'
 
 function App() {
- //Aquí tu código
+  const [name, setName] = useState('Sofía');
+  const [newName, setNewName] = useState('');
+ 
+  const changeName = (e) => {
+    e.preventDefault(); // Evita que se recargue la página al enviar el formulario es decir, previene el comportamiento por defecto de React en formularios.
+    if(newName.trim() !== '') {
+      setName(newName);
+      setNewName('');
+    }
+  };
+
+// --- NOTA --- Hay que llamar a e.preventDefault() dentro de changeName, porque React en formularios recarga la página por defecto.
+
+  return (
+    <div class="listNames">
+      <h2>Teacher Name: {name}</h2>
+      <ul>
+        <li onClick={() => setName('Data')}>Data</li>
+        <br></br>
+        <li onClick={() => setName('Reyes')}>Reyes</li>
+        <br></br>
+        <li onClick={() => setName('Yolanda')}>Yolanda</li>
+      </ul>
+      <div class="changeNameform">
+        <form onSubmit={changeName}>
+          <input type="text" value={newName} onChange={(e) => setNewName(e.target.value)} placeholder="add a name"></input>
+          <button type="submit">Add</button>
+        </form>
+      </div>
+    </div>
+  )
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,18 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
+
+/*
+3. En el JSX del componente, renderiza un título `<h2>` con el texto "Teacher Name" seguido del valor de `name`.
+4. Renderiza una lista `<ul>` con tres elementos `<li>`, cada uno con un nombre de profesor diferente:
+   - El texto de cada elemento `<li>` debe ser uno de los nombres predefinidos ("Data", "Reyes", "Yolanda").
+   - Al hacer clic en cada elemento de la lista, se debe cambiar el nombre del profesor mostrado en el título a uno de los nombres predefinidos correspondientes.
+   - Utiliza la función `setName` proporcionada por `useState` para actualizar el nombre del profesor cuando se haga clic en un elemento de la lista.
+*/
