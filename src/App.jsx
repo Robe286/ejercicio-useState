629f27c22058a8863584d4ab2c6509d214fdc800
Roberto Blasco~ <roberza2202@gmail.com>
2025-06-17 12:04:41 +0200

    addición de comentarios

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,14 @@ function App() {
  
   const changeName = (e) => {
     e.preventDefault(); // Evita que se recargue la página al enviar el formulario es decir, previene el comportamiento por defecto de React en formularios.
-    if(newName.trim() !== '') {
-      setName(newName);
+    if(newName.trim() !== '') { // Verifica que se haya introducido un dato válido es decir, que no hayan espacios en blanco o esté vacío. -- ACLARAR ESTO --
+      setName(newName);         // trim() es un método de string de javaScript que elimina los espacios que puedan haber antes o después del string.
       setNewName('');
     }
   };
 
 // --- NOTA --- Hay que llamar a e.preventDefault() dentro de changeName, porque React en formularios recarga la página por defecto.
-
+// ---ACLARAR--- e.target.value --> ¿que es target?, ¿e es el evento en cuestión o el de envío del formulario?
   return (
     <div class="listNames">
       <h2>Teacher Name: {name}</h2>
